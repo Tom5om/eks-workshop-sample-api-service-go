8e8398f1bde51719714df9477150157cd1ab3c9a
Tom Somerville <tom@somerville.nl>
2019-06-07 14:54:43 +1000

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world - Tom has finally done it")
 	})
 	http.ListenAndServe(":8080", nil)
 }
